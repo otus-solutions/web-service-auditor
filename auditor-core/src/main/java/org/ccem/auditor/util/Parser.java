416b2c2fabc5ebdb96bde9003e236990e6eb54d6
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2017-03-31 16:41:26 -0300

    Remoção atributos restritivos para persistencia

--- auditor-core/src/main/java/org/ccem/auditor/util/Parser.java
+++ auditor-core/src/main/java/org/ccem/auditor/util/Parser.java
@@ -4,16 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.ccem.auditor.model.SessionLog;
+import org.ccem.auditor.processing.PrepareInformation;
 
 public class Parser {
 
-    public static JsonObject body(JsonElement body) {
-        if (!body.isJsonNull()) {
-            JsonObject bodyJsonObject = new Gson().fromJson(body.getAsString(), JsonElement.class).getAsJsonObject();
-            return MaskSensitiveInformation.mask(bodyJsonObject);
-        } else {
-            return new JsonObject();
+    public static JsonObject body(JsonElement element) {
+        if (!element.isJsonNull()) {
+            String body = element.getAsString();
+
+            if (!body.isEmpty()) {
+                JsonObject bodyJsonObject = new Gson().fromJson(body, JsonElement.class).getAsJsonObject();
+                return PrepareInformation.prepare(bodyJsonObject);
+            }
         }
+        return new JsonObject();
     }
 
     public static SessionLog sessionLog(JsonElement sessionLog) {
